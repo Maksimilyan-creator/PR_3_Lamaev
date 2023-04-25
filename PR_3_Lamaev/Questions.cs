694d4cd0f228b5822a4b182b929c1e52ba48ca83
maksimilyan.lamaev.13@inbox.ru <maksimilyan.lamaev.13@inbox.ru>
2023-04-25 15:44:53 +0700

    /

---
+++ PR_3_Lamaev/Questions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_3_Lamaev
+{
+    internal class Questions
+    {
+        public string question { get; set; }
+        public string answer { get; set; }
+        public int Point { get; set; }
+        public Questions( string questoins, string answers, int points ) 
+        {
+            question = questoins;
+            answer = answers;
+            Point = points;
+        }
+    }
+}
